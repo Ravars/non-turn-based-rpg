c01f59ee7923aff2e0a5c515b855a3e9b990f48c
Vitor Soares Silva <vitrola.vitor@gmail.com>
2025-10-30 14:11:10 +0000

    Fix AbilityButton

--- Scripts/UI/ability_button.gd
+++ Scripts/UI/ability_button.gd
@@ -16,7 +16,7 @@ func _ready():
 	gui_input.connect(_on_gui_input)
 
 func _get_drag_data(_at_position: Vector2) -> Variant:
-	if timeline_ui and timeline_ui.is_selecting_target:
+	if TimelineManager.is_selecting_target:
 		print("Bloqueado: Selecione um alvo primeiro")
 		return null
 	if hero_owner.is_dead:
