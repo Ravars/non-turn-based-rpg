8f205a8b1efd22359c0c1aa8a9616d5ec78130ba
Vitor Soares Silva <vitrola.vitor@gmail.com>
2025-11-12 19:19:59 +0000

    Fix Remove skill from loop

--- Scripts/UI/skill_loop_panel.gd
+++ Scripts/UI/skill_loop_panel.gd
@@ -47,16 +47,19 @@ func update_ui():
 	var current_total_cast_time = 0.0
 	var current_total_idle_time = 0.0
 	var current_total_duration = 0.0
+	var skill_loop_index_counter = 0
 	for step in selected_hero.execution_plan:
 		if step.type == "cast":
 			var skill = step.skill
 			var item: SkillLoopItem = SkillLoopItemScene.instantiate()
 			# Encontra o índice da skill no skill_loop original para o botão de remover
-			var original_index = selected_hero.skill_loop.find(skill)
-			item.setup(skill, original_index, true, 0.0)
+			# var original_index = selected_hero.skill_loop.find(skill)
+			var index_to_remove = skill_loop_index_counter
+			item.setup(skill, index_to_remove, true, 0.0)
 			item.action_button_pressed.connect(_on_remove_skill_pressed)
 			loop_container.add_child(item)
 			current_total_cast_time += skill.cast_time
+			skill_loop_index_counter += 1
 		elif step.type == "idle":
 			# Para o tempo ocioso, podemos adicionar um item visual diferente ou apenas somar ao total
 			var idle_duration = step.duration
@@ -81,5 +84,6 @@ func _on_add_skill_pressed(skill_data: SkillData, _index: int):
 
 func _on_remove_skill_pressed(_skill_data: SkillData, index: int):
 	if is_instance_valid(selected_hero):
+		print("Index {0}".format({0: index}))
 		selected_hero.remove_skill_from_loop(index)
 		update_ui()
